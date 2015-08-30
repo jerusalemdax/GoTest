78b6257d7880047ad342ef3297a3bba3f2ea4e8f
jerusalemdax <jerusalemdax@gmail.com>
2015-08-31 07:59:21 +0800

    增加命令执行测试和平台区分测试

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -22,4 +24,19 @@ func main() {
 	fmt.Println("t参数为: %s", *str)
 	fmt.Println("n参数为: %d", *num)
 	fmt.Println("b参数为: %t", *bo)
+
+	fmt.Println("执行命令测试用例")
+	fmt.Println("执行ls -al命令")
+	cmd := exec.Command("ls", "-al")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "执行命令错误: %s ", err)
+		return
+	}
+	fmt.Fprintf(os.Stdout, "执行结果: %s", out)
+
+	fmt.Println("区分不同平台测试")
+	fmt.Println(runtime.GOOS)
+
+	fmt.Println("protobuf测试")
 }
